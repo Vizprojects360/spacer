cdceffaf2a0abf589778ac3f94c232bfe6519656
Vizprojects360 <155273263+Vizprojects360@users.noreply.github.com>
2024-10-17 00:40:14 +0200

    Commit

---
+++ data.js
@@ -0,0 +1,334 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-balkon",
+      "name": "Balkon",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2752,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5173503985447576,
+          "pitch": 0.575066750398955,
+          "rotation": 0,
+          "target": "5-salon_2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-kuchnia",
+      "name": "Kuchnia",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2752,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.41368475328641807,
+          "pitch": 0.618920022463854,
+          "rotation": 0,
+          "target": "5-salon_2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-azienka",
+      "name": "Łazienka",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2752,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-przedpokj",
+      "name": "Przedpokój",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2752,
+      "initialViewParameters": {
+        "yaw": -2.6493005135674856,
+        "pitch": 0.31834420420777576,
+        "fov": 1.4628963779807613
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.18655454104452218,
+          "pitch": 0.4796990721810115,
+          "rotation": 0,
+          "target": "6-sypialnia"
+        },
+        {
+          "yaw": 3.0160146208368745,
+          "pitch": 0.6701859459995205,
+          "rotation": 0,
+          "target": "4-salon_1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-salon_1",
+      "name": "Salon_1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2752,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.4811660115520713,
+          "pitch": 0.33448420847066807,
+          "rotation": 0,
+          "target": "2-azienka"
+        },
+        {
+          "yaw": -0.11831428141531397,
+          "pitch": 0.6202276640784898,
+          "rotation": 0,
+          "target": "3-przedpokj"
+        },
+        {
+          "yaw": 2.485939110309225,
+          "pitch": 0.6325568827545975,
+          "rotation": 0,
+          "target": "5-salon_2"
+        },
+        {
+          "yaw": -2.874272557315738,
+          "pitch": 0.5674622027497787,
+          "rotation": 0,
+          "target": "1-kuchnia"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-salon_2",
+      "name": "Salon_2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2752,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7005338593149482,
+          "pitch": 0.5715552097137078,
+          "rotation": 0,
+          "target": "4-salon_1"
+        },
+        {
+          "yaw": 0.675120508896093,
+          "pitch": 0.480314459921221,
+          "rotation": 0,
+          "target": "1-kuchnia"
+        },
+        {
+          "yaw": -1.220696059443064,
+          "pitch": 0.6418276209136149,
+          "rotation": 0,
+          "target": "0-balkon"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-sypialnia",
+      "name": "Sypialnia",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2752,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.36042534865120146,
+          "pitch": 0.28333541281267394,
+          "rotation": 0,
+          "target": "3-przedpokj"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Mieszkanie ",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
